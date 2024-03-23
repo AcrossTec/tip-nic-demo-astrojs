9042ce657d22ef5538b62ac586e5b69c7af8a74a
Johan Paiz <johanedfb@gmail.com>
2024-03-23 17:29:21 -0600

    feat: Add Reservation base logic

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -1,22 +1,4 @@
-export type Products = Product[];
-export interface Product {
-	name: string;
-	category: string;
-	technology: string;
-	id: number;
-	description: string;
-	price: string;
-	discount: string;
-}
-
-export type Users = User[];
-export interface User {
-	id: number;
-	name: string;
-	avatar: string;
-	email: string;
-	biography: string;
-	position: string;
-	country: string;
-	status: string;
+export type Reservations = Reservation[];
+export interface Reservation {
+	
 }
