7c6819512d201f749af0ff0773b1afbd2d6942b3
Johan Paiz <johanedfb@gmail.com>
2024-03-24 15:50:44 -0600

    feat: Add Insert Reservations

--- src/services/reservations.ts
+++ src/services/reservations.ts
@@ -1,3 +1,22 @@
-export function getReservations() {
-		return [];
+import type { IReservation, Reservations } from "../types/entities.ts";
+
+let ReservationList: Reservations = []
+
+export function getReservations(): Reservations {
+	return ReservationList;
+}
+
+export function insertReservation(reservation: IReservation) {
+	// eslint-disable-next-line no-param-reassign
+	reservation.reservationId = `RESV-0${ReservationList.length + 1}`;
+	
+	ReservationList.push(reservation);
+}
+
+export function deleteReservation(reservationId: string) {
+	ReservationList = ReservationList.filter((reservation) => reservation.reservationId !== reservationId);
+}
+
+export function updateReservation(reservation: IReservation) {
+	ReservationList = ReservationList.map((r) => r.reservationId === reservation.reservationId ? reservation : r);
 }
--- src/types/entities.ts
+++ src/types/entities.ts
@@ -1,4 +1,16 @@
-export type Reservations = Reservation[];
-export interface Reservation {
-	
+export type Reservations = IReservation[];
+
+export type IReservation =
+{
+	reservationId: string
+	name: string;	
+	payment: string;
+	commission: string;
+	customerServices: string;
+	date: string;
+	pax: string;
+	observations: string;
+	tour: string;
+	time: string;
+	withGuide: boolean;
 }
