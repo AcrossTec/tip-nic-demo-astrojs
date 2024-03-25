76fecb1cdb80b988fc436a4cc9c19242dc079be9
Johan Paiz <johanedfb@gmail.com>
2024-03-24 18:34:15 -0600

    feat: Now reservations are save in the local storage and add get reservations module.

--- src/services/reservations.ts
+++ src/services/reservations.ts
@@ -1,22 +1,42 @@
 import type { IReservation, Reservations } from "../types/entities.ts";
 
-let ReservationList: Reservations = []
-
-export function getReservations(): Reservations {
-	return ReservationList;
+export function getReservations(): Reservations
+{
+	const reservations = window.localStorage.getItem('reservations');
+	if (reservations)
+	{
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+		return JSON.parse(reservations);
+	}
+	return [];
 }
 
-export function insertReservation(reservation: IReservation) {
+export function insertReservation(reservation: IReservation)
+{
+	const ReservationList = getReservations();
+
 	// eslint-disable-next-line no-param-reassign
 	reservation.reservationId = `RESV-0${ReservationList.length + 1}`;
-	
+
+	// save in the localstorage browser
 	ReservationList.push(reservation);
+	const jsonString = JSON.stringify(ReservationList);
+
+	window.localStorage.setItem('reservations', jsonString);
 }
 
-export function deleteReservation(reservationId: string) {
-	ReservationList = ReservationList.filter((reservation) => reservation.reservationId !== reservationId);
+export function deleteReservation(reservationId: string)
+{
+	let ReservationList = getReservations();
+	ReservationList = ReservationList.filter((r) => r.reservationId !== reservationId);
+	const jsonString = JSON.stringify(ReservationList);
+	window.localStorage.setItem('reservations', jsonString);
 }
 
-export function updateReservation(reservation: IReservation) {
-	ReservationList = ReservationList.map((r) => r.reservationId === reservation.reservationId ? reservation : r);
+export function updateReservation(reservation: IReservation)
+{
+	let ReservationList = getReservations();
+	ReservationList = ReservationList.map((r) => (r.reservationId === reservation.reservationId ? reservation : r));
+	const jsonString = JSON.stringify(ReservationList);
+	window.localStorage.setItem('reservations', jsonString);
 }
